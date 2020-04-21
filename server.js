131b6b4c86867fcefc2277b6cdf8fcb4b7d26c2d
cafesao <gabriel.diasdutra2@gmail.com>
2020-04-21 16:06:06 -0300

    Modificando arquivo JS principal

--- server.js
+++ server.js
@@ -3,13 +3,18 @@ require('marko/node-require.js').install()
 
 const markoExpress = require('marko/express.js')
 const express = require('express')
+const bodyParser = require('body-parser')
 const routes = require('./src/app/routes/routes.js')
 
 //Constantes
 const server = express()
 
 //Codigo
-server.use(express.json())
+server.use(
+  bodyParser.urlencoded({
+    extended: true,
+  }),
+)
 server.use(markoExpress())
 server.use('/api', routes)
 
