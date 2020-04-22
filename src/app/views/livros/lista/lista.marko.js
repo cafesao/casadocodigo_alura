8dce07bdb0bf3cf15173c6b059b6177cb3ce3db2
cafesao <gabriel.diasdutra2@gmail.com>
2020-04-22 14:45:45 -0300

    Modificando arquivos .marko

--- src/app/views/livros/lista/lista.marko.js
+++ src/app/views/livros/lista/lista.marko.js
@@ -7,6 +7,7 @@ var marko_template = module.exports = require("marko/src/html").t(__filename),
     marko_forOf = require("marko/src/runtime/helpers/for-of"),
     helpers_escape_xml = require("marko/src/runtime/html/helpers/escape-xml"),
     marko_escapeXml = helpers_escape_xml.x,
+    marko_attr = require("marko/src/runtime/html/helpers/attr"),
     marko_loadTag = require("marko/src/runtime/helpers/load-tag"),
     init_components_tag = marko_loadTag(require("marko/src/core-tags/components/init-components-tag")),
     await_reorderer_tag = marko_loadTag(require("marko/src/core-tags/core/await/reorderer-renderer")),
@@ -15,25 +16,33 @@ var marko_template = module.exports = require("marko/src/html").t(__filename),
 function render(input, out, __component, component, state) {
   var data = input;
 
-  out.w("<html lang=\"pt-br\"><head><meta charset=\"UTF-8\"><meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\"><title>Casa do Codigo</title></head><body><table><tr><td>ID</td><td>Titulo</td></tr>");
+  out.w("<html lang=\"pt-br\"><head><meta charset=\"UTF-8\"><meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\"><title>Casa do Codigo</title></head><body><table id=\"livros\"><tr><td>ID</td><td>Titulo</td><td>Preço</td><td>Editar</td><td>Remover</td></tr>");
 
   var $for$0 = 0;
 
   marko_forOf(data.livros, function(livro) {
     var $keyScope$0 = "[" + (($for$0++) + "]");
 
-    out.w("<tr><td>" +
+    out.w("<tr" +
+      marko_attr("id", "livro_" + livro.id) +
+      "><td>" +
       marko_escapeXml(livro.id) +
       "</td><td>" +
       marko_escapeXml(livro.titulo) +
-      "</td></tr>");
+      "</td><td>" +
+      marko_escapeXml(livro.preco) +
+      "</td><td><a" +
+      marko_attr("href", "/api/livros/form/" + livro.id) +
+      ">Editar</a></td><td><a href=\"#\"" +
+      marko_attr("data-ref", "" + livro.id) +
+      " data-type=\"remocao\">Remover</a></td></tr>");
   });
 
-  out.w("</table>");
+  out.w("</table><script src=\"/estatico/js/remove-livro.js\"></script>");
 
   init_components_tag({}, out);
 
-  await_reorderer_tag({}, out, __component, "13");
+  await_reorderer_tag({}, out, __component, "21");
 
   _preferred_script_location_tag({}, out);
 
